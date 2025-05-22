4e63b37816dbfd5ad2c2f35f8a8b17a9e4eaa976
Fernando Velazquez <jfernando.velazquez@live.com.mx>
2025-05-22 17:14:37 +0200

    Update mistral_runner.py

--- runners/mistral_runner.py
+++ runners/mistral_runner.py
@@ -1,21 +1,3 @@
-# Model Configuration for Mistral-7B-Instruct-v0.2
-# -----------------------------------------------
-# model: Mistral-7B-Instruct-v0.2
-# https://gitlab.cee.redhat.com/models-corp/user-documentation/-/blob/main/models/mistral-7b-instruct-v0-2.md?ref_type=heads
-# model-string: mistralai/Mistral-7B-Instruct-v0.2
-# api: https://mistral-7b-instruct-v0-2--apicast-production.apps.int.stc.ai.prod.us-east-1.aws.paas.redhat.com/v1
-# huggingface: https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
-# internal-repository: https://simple-llm-cache.apps.int.stc.ai.prod.us-east-1.aws.paas.redhat.com/llm-cache/Mistral-7B-Instruct-v0.2/
-# context-length: deployed with 16384 of model theoretical max 32768
-# tool-calling: No
-# tool-calling-template: na
-# supported-criticality: C4
-# availability-slo: 95%
-# redundant-deployment: false
-# disaster-recovery: false
-# cost-per-million-inferences: $0.0  <- cost model not implemented yet
-
-
 import os
 import requests
 
@@ -24,7 +6,7 @@ class MistralInterface:
         self.api_url = "https://mistral.foo.com”
         self.model_id = "mistralai/Mistral"
         self.api_key = os.getenv("USER_KEY")
-        
+
         if not self.api_key:
             raise ValueError("USER_KEY environment variable not set")
 
